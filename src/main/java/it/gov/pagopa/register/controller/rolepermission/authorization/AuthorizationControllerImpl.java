1ad5ccf9f45549e30d06bdfdb855e5c36212e16e
DanieleRanaldo <Daniele.Ranaldo@nttdata.com>
2025-06-11 16:07:37 +0200

    [RDB-33] added controller and updated pom

---
+++ src/main/java/it/gov/pagopa/register/controller/rolepermission/authorization/AuthorizationControllerImpl.java
@@ -0,0 +1,25 @@
+package it.gov.pagopa.register.controller.rolepermission.authorization;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import it.gov.pagopa.register.dto.rolepermission.UserPermissionDTO;
+import it.gov.pagopa.register.service.rolepermission.RolePermissionService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class AuthorizationControllerImpl implements AuthorizationController {
+
+    private final RolePermissionService rolePermissionService;
+
+    public AuthorizationControllerImpl(RolePermissionService rolePermissionService) {
+        this.rolePermissionService = rolePermissionService;
+    }
+
+    @Override
+    public ResponseEntity<UserPermissionDTO> getUserPermissions(String institution) throws JsonProcessingException {
+        UserPermissionDTO userPermissionDTO = rolePermissionService.getUserPermission(institution);
+        return new ResponseEntity<>(userPermissionDTO, HttpStatus.OK);
+    }
+
+}
