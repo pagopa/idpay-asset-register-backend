d97b515df6418935549731a5f955485e0e40779c
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-06-18 11:04:08 +0200

    feign client update

--- src/main/java/it/gov/pagopa/register/connector/onetrust/OneTrustRestClient.java
+++ src/main/java/it/gov/pagopa/register/connector/onetrust/OneTrustRestClient.java
@@ -7,13 +7,14 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "app.rest-client.one-trust.service.name", url = "${app.rest-client.one-trust.service.privacy-notices.base-url}")
+@FeignClient(name = "${app.rest-client.one-trust.service.name}", url = "${app.rest-client.one-trust.service.privacy-notices.base-url}")
 public interface OneTrustRestClient {
 
-    @GetMapping(value = "/privacynotices/{id}")
-    PrivacyNoticesDTO getPrivacyNotices(
-            @PathVariable("id") String id,
-            @RequestParam String date,
-            @RequestHeader("Authorization") String bearer);
+  @GetMapping(value = "/privacynotices/{id}")
+  PrivacyNoticesDTO getPrivacyNotices(
+    @PathVariable("id") String id,
+    @RequestParam String date,
+    @RequestHeader("Authorization") String bearer);
 
 }
+
