e0862d4950eeea3801c834530446df3917a019f8
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-06-20 16:07:04 +0200

    upload update

--- src/main/java/it/gov/pagopa/register/service/operation/ProductService.java
+++ src/main/java/it/gov/pagopa/register/service/operation/ProductService.java
@@ -1,7 +1,5 @@
 package it.gov.pagopa.register.service.operation;
 
-import com.azure.core.http.rest.Response;
-import com.azure.storage.blob.models.BlobProperties;
 import it.gov.pagopa.register.connector.onetrust.InitiativeFileStorageClient;
 import it.gov.pagopa.register.constants.enums.UploadCsvStatus;
 import it.gov.pagopa.register.dto.operation.RegisterUploadReqeustDTO;
@@ -9,24 +7,19 @@ import it.gov.pagopa.register.exception.operation.CsvValidationException;
 import it.gov.pagopa.register.exception.operation.ReportNotFoundException;
 import it.gov.pagopa.register.model.operation.UploadCsv;
 import it.gov.pagopa.register.repository.operation.UploadRepository;
-import it.gov.pagopa.register.utils.ReportError;
-import it.gov.pagopa.register.utils.ValidationError;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.csv.CSVFormat;
 import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVPrinter;
 import org.apache.commons.csv.CSVRecord;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.nio.file.Path;
+import java.io.*;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import static it.gov.pagopa.register.utils.Utils.*;
 
@@ -47,13 +40,13 @@ public class ProductService {
 
 
   public void saveCsv(RegisterUploadReqeustDTO registerUploadReqeustDTO, String idOrg, String idUser, String role) {
-        /*
-          1. CONTROLLI CHE BLOCCO IL CONTROLLO DEL CONTENUTO
+  /*
+    1. CONTROLLI CHE BLOCCO IL CONTROLLO DEL CONTENUTO
 
-          1. Controllo estensione
-          2. Controllo prima riga come header
-          3. Controllo numero righe csvParser.getRecords().size();
-         */
+    1. Controllo estensione
+    2. Controllo prima riga come header
+    3. Controllo numero righe csvParser.getRecords().size();
+   */
 
     //2. CONTROLLI DEL CONTENUTO
 
@@ -63,20 +56,16 @@ public class ProductService {
     if (!isCsv(registerUploadReqeustDTO.getCsv()))
       throw new CsvValidationException("Il file inserito non è un .csv");
 
+    // settere ; come separatore
     try (BufferedReader reader = new BufferedReader(new InputStreamReader(registerUploadReqeustDTO.getCsv().getInputStream()));
-
-         //settare ; come separator
-         CSVParser csvParser = new CSVParser(reader,
-           CSVFormat.Builder.create().setHeader().setTrim(true).build())) {
-
+         CSVParser csvParser = new CSVParser(reader, CSVFormat.Builder.create().setHeader().setTrim(true).build())){
 
       Boolean isPianoCottura = checkCategory(CATEGORIE_PIANI_COTTURA, registerUploadReqeustDTO.getCategory());
       //check headers
-      if ((isPianoCottura && !CSV_HEADER_PIANI_COTTURA.containsAll(csvParser.getHeaderMap().keySet()))
-        || (!isPianoCottura && !CSV_HEADER_PRODOTTI.containsAll(csvParser.getHeaderMap().keySet()))) {
-        throw new CsvValidationException("header csv non validi");
-      }
-
+      if ((isPianoCottura && !new HashSet<>(CSV_HEADER_PIANI_COTTURA).containsAll(csvParser.getHeaderMap().keySet())) ||
+          (!isPianoCottura && !new HashSet<>(CSV_HEADER_PRODOTTI).containsAll(csvParser.getHeaderMap().keySet())))
+            throw new CsvValidationException("header csv non validi");
+      List<String> csvHeader = new ArrayList<>(isPianoCottura ? CSV_HEADER_PIANI_COTTURA : CSV_HEADER_PRODOTTI);
 
       //check numero righe
       //da spostare nell'appProperties
@@ -84,35 +73,30 @@ public class ProductService {
         throw new CsvValidationException("numero di record nel csv maggiore di " + MAX_ROWS);
 
       //GENERARE idUpload idOrg +category+ userId + timestamp
-      String idUpload = idOrg + "-" + registerUploadReqeustDTO.getCategory() + "-" + idUser + "-" +LocalDateTime.now().toString();
-
+      String idUpload = idOrg + "-" + registerUploadReqeustDTO.getCategory() + "-" + idUser + "-" + LocalDateTime.now();
 
       //Check contenuto
-      ReportError errors = new ReportError(idUpload, new ArrayList<>());
-      List <String> messageErrors = new ArrayList<>();
-
-      List<String> errorHeaders = new ArrayList<>();
-      if(isPianoCottura){
-        errorHeaders.addAll(CSV_HEADER_PIANI_COTTURA);
-      }else{
-        errorHeaders = new ArrayList(CSV_HEADER_PRODOTTI);
-      }
-
-      errorHeaders.add("Errori");
-
-      List errorRecords = new ArrayList<>();
-
-
+      Map<String, String> rowWithErrors = new LinkedHashMap<>();
       for (CSVRecord record : csvParser) {
+        List<String> errors;
+        String categoriaValue = record.get("Categoria");
 
-        if (isPianoCottura) {
-          List<ValidationError> errorList = checkPianiCotturaCsvRow(record);
-          errors.getErrors().addAll(errorList);
+        if ("COOKINGHOBS".equalsIgnoreCase(categoriaValue)) {
+          errors = checkPianiCotturaCsvRow(record);
         } else {
-          List<ValidationError> errorList = checkProdottiCsvRow(record);
-          errors.getErrors().addAll(errorList);
+          errors = checkProdottiCsvRow(record, registerUploadReqeustDTO.getCategory());
+        }
+
+
+        if (!errors.isEmpty()) {
+          csvHeader.add("Errori");
+          for (String header : csvHeader) {
+            rowWithErrors.put(header, record.get(header));
+          }
+          rowWithErrors.put("Errori", String.join(", ", errors));
         }
       }
+
 
       String fileName = registerUploadReqeustDTO.getCsv().getResource().getFile().getName();
 
@@ -127,15 +111,15 @@ public class ProductService {
         null,
         fileName);
 
-
-      if (errors.getErrors().isEmpty()) {
+      //se non ci sono stati errori
+      if (rowWithErrors.isEmpty()) {
 
         // 1.1 Salvo a db i dati legati al caricamento è quelli necessari al successivo downlaod
         uploadFile.setStatus(UploadCsvStatus.FORMAL_OK.toString());
         uploadRepository.save(uploadFile);
 
         //1.2 Carico il file sullo storage di azure
-        String destination = "/CSV/" + idOrg + "/";
+        String destination = "CSV/" + idOrg + "/";
         azureBlobClient.uploadFile(registerUploadReqeustDTO.getCsv().getResource().getFile(),
           destination,
           registerUploadReqeustDTO.getCsv().getContentType());
@@ -146,19 +130,23 @@ public class ProductService {
                 String statusUpload = QUALCOSA DEL TIPO PRESA IN CARICO;
                 Status 200
                */
-      } else {
+      }
+      else {
+        try (BufferedWriter writer = Files.newBufferedWriter(Paths.get(idUpload + ".csv"));
+             CSVPrinter csvPrinter = new CSVPrinter(writer, CSVFormat.Builder.create().setHeader(csvHeader.toArray(new String[0])).setTrim(true).build())) {
+          csvPrinter.printRecord(rowWithErrors.values());
 
-        // 2.1 Salvo a db i dati legati al caricamento è quelli necessari al successivo downlaod
-        uploadFile.setStatus(UploadCsvStatus.FORMAL_KO.toString());
-        uploadRepository.save(uploadFile);
+          // 2.1 Salvo a db i dati legati al caricamento è quelli necessari al successivo downlaod
+          uploadFile.setStatus(UploadCsvStatus.FORMAL_KO.toString());
+          uploadRepository.save(uploadFile);
 
-        //2.2 Geniro il file di report e carico il file sullo storage di azure
+          //2.2 Geniro il file di report e carico il file sullo storage di azure
 
-        String destination = "/Report/Eprel_Error/";
-        // ReportError da cambiare in MultipartFile? (1 riga per prodotto)
-        azureBlobClient.uploadFile(null,
-          destination,
-          registerUploadReqeustDTO.getCsv().getContentType());
+          String destination = "Report/Eprel_Error/";
+          // ReportError da cambiare in MultipartFile? (1 riga per prodotto)
+          azureBlobClient.uploadFile(null,
+            destination,
+            registerUploadReqeustDTO.getCsv().getContentType());
 
              /*
                 2.3 Restituo il seguento body e code
@@ -166,72 +154,67 @@ public class ProductService {
                 String statusUpload = VALIDATION_ERROR;
                 Status 500
               */
-
+        }
+        catch (IOException e) {
+          throw new RuntimeException("Errore nella scrittura del file CSV di report: " + e.getMessage());
+        }
       }
-
-
-    } catch (IOException e) {
+    }
+    catch (IOException e) {
       throw new RuntimeException("Errore nella lettura del file CSV: " + e.getMessage());
     }
   }
 
-    private Boolean checkCategory(List<String> listaCategorie, String categoria){
-      return listaCategorie.contains(categoria);
-    }
+  private Boolean checkCategory(List<String> listaCategorie, String categoria){
+    return listaCategorie.contains(categoria);
+  }
 
-    private Boolean isCsv(MultipartFile file){
-      if (file == null || file.isEmpty() || file.getOriginalFilename() == null || file.getOriginalFilename().toLowerCase().endsWith(".csv") ) {
-        return false;
-      }
+  private Boolean isCsv(MultipartFile file){
+      return file != null && !file.isEmpty() && file.getOriginalFilename() != null && !file.getOriginalFilename().toLowerCase().endsWith(".csv");
+  }
 
-      return true;
-    }
-
-    private List<ValidationError> checkPianiCotturaCsvRow(CSVRecord csvRecord) {
-            List<ValidationError> errors = new ArrayList<>();
-            int rowCount = 0;
-            if (!csvRecord.get("Codice GTIN/EAN").matches(CODICE_GTIN_EAN_REGEX)) {
-                errors.add(new ValidationError(rowCount, "codiceGTIN/EAN", "Il Codice GTIN/EAN è obbligatorio, deve essere univoco, alfanumerico e lungo al massimo 14 caratteri."));
-            }
-            if (!CATEGORIE_PIANI_COTTURA.contains(csvRecord.get("Categoria"))) {
-                errors.add(new ValidationError(rowCount, "categoria", "Il campo Categoria è obbligatorio e deve contenere il valore fisso 'COOKINGHOBS'."));
-            }
-            if (!csvRecord.get("Marca").matches(MARCA_REGEX)) {
-                errors.add(new ValidationError(rowCount, "marca", "Il campo Marca è obbligatorio e deve contenere una stringa lunga al massimo 100 caratteri."));
-            }
-            if (!csvRecord.get("Modello").matches(MODELLO_REGEX)) {
-                errors.add(new ValidationError(rowCount, "modello", "Il campo Modello è obbligatorio e deve contenere una stringa lunga al massimo 100 caratteri."));
-            }
-            if (!csvRecord.get("Codice prodotto").matches(CODICE_PRODOTTO_REGEX)) {
-                errors.add(new ValidationError(rowCount, "codiceProdotto", "Il Codice prodotto non deve contenere caratteri speciali (come &, ', <, >, %, $, :, ;, =) o lettere accentate e deve essere lungo al massimo 100 caratteri."));
-            }
-            if (!csvRecord.get("Paese di Produzione").matches(PAESE_DI_PRODUZIONE_REGEX)) {
-              errors.add(new ValidationError(rowCount, "codiceProdotto", "Il Paese di Produzione è obbligatorio e deve essere composto da esattamente 2 caratteri"));
-            }
-        return errors;
-    }
-
-    private List<ValidationError> checkProdottiCsvRow(CSVRecord csvRecord) {
-      List<ValidationError> errors = new ArrayList<>();
-      int rowCount = 0;
-      if (!csvRecord.get("Codice EPREL").matches(CODICE_EPREL_REGEX)) {
-        errors.add(new ValidationError(rowCount, "codiceEPREL", "Il Codice EPREL è obbligatorio e deve essere un valore numerico."));
-      }
-      if (!csvRecord.get("Codice GTIN/EAN").matches(CODICE_GTIN_EAN_REGEX)) {
-        errors.add(new ValidationError(rowCount, "codiceGTIN/EAN", "Il Codice GTIN/EAN è obbligatorio, deve essere univoco, alfanumerico e lungo al massimo 14 caratteri."));
-      }
-      if (!CATEGORIE_PRODOTTI.contains(csvRecord.get("Categoria"))) {
-        errors.add(new ValidationError(rowCount, "categoria", "Il campo Categoria è obbligatorio e deve contenere uno dei seguenti valori: WASHINGMACHINES, WASHDRYERS, OVENS, RANGEHOODS, DISHWASHERS, TUMBLEDRIERS, REFRIGERATINGAPPL."));
-      }
-      if (!csvRecord.get("Codice prodotto").matches(CODICE_PRODOTTO_REGEX)) {
-        errors.add(new ValidationError(rowCount, "codiceProdotto", "Il Codice prodotto non deve contenere caratteri speciali (come &, ', <, >, %, $, :, ;, =) o lettere accentate e deve essere lungo al massimo 100 caratteri."));
-      }
-      if (!csvRecord.get("Paese di Produzione").matches(PAESE_DI_PRODUZIONE_REGEX)) {
-        errors.add(new ValidationError(rowCount, "codiceProdotto", "Il Paese di Produzione è obbligatorio e deve essere composto da esattamente 2 caratteri"));
-      }
+  private List<String> checkPianiCotturaCsvRow(CSVRecord csvRecord) {
+          List<String> errors = new ArrayList<>();
+          if (!csvRecord.get("Codice GTIN/EAN").matches(CODICE_GTIN_EAN_REGEX)) {
+              errors.add( "Il Codice GTIN/EAN è obbligatorio e deve essere univoco ed alfanumerico e lungo al massimo 14 caratteri");
+          }
+          if (!CATEGORIE_PIANI_COTTURA.contains(csvRecord.get("Categoria"))) {
+              errors.add("Il campo Categoria è obbligatorio e deve contenere il valore fisso 'COOKINGHOBS'");
+          }
+          if (!csvRecord.get("Marca").matches(MARCA_REGEX)) {
+              errors.add("Il campo Marca è obbligatorio e deve contenere una stringa lunga al massimo 100 caratteri");
+          }
+          if (!csvRecord.get("Modello").matches(MODELLO_REGEX)) {
+              errors.add("Il campo Modello è obbligatorio e deve contenere una stringa lunga al massimo 100 caratteri");
+          }
+          if (!csvRecord.get("Codice prodotto").matches(CODICE_PRODOTTO_REGEX)) {
+              errors.add( "Il Codice prodotto non deve contenere caratteri speciali o lettere accentate e deve essere lungo al massimo 100 caratteri");
+          }
+          if (!csvRecord.get("Paese di Produzione").matches(PAESE_DI_PRODUZIONE_REGEX)) {
+            errors.add( "Il Paese di Produzione è obbligatorio e deve essere composto da esattamente 2 caratteri");
+          }
       return errors;
-    }
+  }
 
+  private List<String> checkProdottiCsvRow(CSVRecord csvRecord, String category) {
+    List<String> errors = new ArrayList<>();
+    if (!csvRecord.get("Codice EPREL").matches(CODICE_EPREL_REGEX)) {
+      errors.add("Il Codice EPREL è obbligatorio e deve essere un valore numerico");
+    }
+    if (!csvRecord.get("Codice GTIN/EAN").matches(CODICE_GTIN_EAN_REGEX)) {
+      errors.add("Il Codice GTIN/EAN è obbligatorio e deve essere univoc ed alfanumerico e lungo al massimo 14 caratteri");
+    }
+    if (!CATEGORIE_PRODOTTI.contains(csvRecord.get("Categoria"))) {
+      errors.add("Il campo Categoria è obbligatorio e deve contenere il valore fisso "+category);
+    }
+    if (!csvRecord.get("Codice prodotto").matches(CODICE_PRODOTTO_REGEX)) {
+      errors.add("Il Codice prodotto non deve contenere caratteri speciali o lettere accentate e deve essere lungo al massimo 100 caratteri");
+    }
+    if (!csvRecord.get("Paese di Produzione").matches(PAESE_DI_PRODUZIONE_REGEX)) {
+      errors.add("Il Paese di Produzione è obbligatorio e deve essere composto da esattamente 2 caratteri");
+    }
+    return errors;
+  }
 
   public ByteArrayOutputStream downloadReport(String idUpload) {
     UploadCsv upload = uploadRepository.findByIdUpload(idUpload)
