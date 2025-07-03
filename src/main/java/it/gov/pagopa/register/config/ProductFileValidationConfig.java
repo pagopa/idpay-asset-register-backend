c7be6d7b651f164758cba016069d9ffee12838cb
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-07-03 10:48:24 +0200

    get maxrows from application properties

--- src/main/java/it/gov/pagopa/register/config/ProductFileValidationConfig.java
+++ src/main/java/it/gov/pagopa/register/config/ProductFileValidationConfig.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.register.config;
 
 import it.gov.pagopa.register.service.operation.ColumnValidationRule;
 import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
@@ -14,6 +15,7 @@ import java.util.Map;
 public class ProductFileValidationConfig {
 
 
+  @Value("${product-file-validation.maxRows}")
   private int maxRows;
 
   private Map<String, LinkedHashMap<String, ColumnValidationRule>> schemas = initialize();
