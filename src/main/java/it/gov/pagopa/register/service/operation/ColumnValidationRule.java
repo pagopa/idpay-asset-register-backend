4bf4987b6ba8ef67f9809a60fd8417be22d16596
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2025-07-03 10:38:54 +0200

    refactor validation map

--- src/main/java/it/gov/pagopa/register/service/operation/ColumnValidationRule.java
+++ src/main/java/it/gov/pagopa/register/service/operation/ColumnValidationRule.java
@@ -1,14 +1,18 @@
 package it.gov.pagopa.register.service.operation;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
+import java.util.function.Predicate;
+
 @Data
+@AllArgsConstructor
 public class ColumnValidationRule {
 
-  private String regex;
+  private Predicate<String> rule;
   private String message;
 
   public boolean isValid(String value) {
-    return value != null && value.matches(regex);
+    return value != null && rule.test(value);
   }
 }
