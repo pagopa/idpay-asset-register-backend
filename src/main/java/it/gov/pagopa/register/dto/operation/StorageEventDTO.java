cb9321ebdcf749d0f9ecdacdbdd26fd0555c18bd
dalilaacqua <dalila.acquaviva@nttdata.com>
2025-10-08 18:15:23 +0200

    fix: add topic to kafka event (#84)
    Co-authored-by: DalilaAcq <acquavivadalila@gmail.com>

--- src/main/java/it/gov/pagopa/register/dto/operation/StorageEventDTO.java
+++ src/main/java/it/gov/pagopa/register/dto/operation/StorageEventDTO.java
@@ -15,6 +15,7 @@ public class StorageEventDTO {
     private String id;
     private String subject;
     private String eventType;
+    private String topic;
     private StorageEventData data;
     private LocalDateTime eventTime;
 
