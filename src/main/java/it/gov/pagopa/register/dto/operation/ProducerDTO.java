41f64532f9d01436fe694f732c595adb3c237999
dalilaacqua <dacquavi@emeal.nttdata.com>
2026-05-29 17:29:12 +0200

    feat: add getProducers API with pagination support and DTOs

---
+++ src/main/java/it/gov/pagopa/register/dto/operation/ProducerDTO.java
@@ -0,0 +1,19 @@
+package it.gov.pagopa.register.dto.operation;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProducerDTO {
+
+  private String producerName;
+  private LocalDateTime createdAt;
+  private LocalDateTime updatedAt;
+}
--- src/main/java/it/gov/pagopa/register/repository/operation/ProducersInitiativeRepository.java
+++ src/main/java/it/gov/pagopa/register/repository/operation/ProducersInitiativeRepository.java
@@ -1,6 +1,8 @@
 package it.gov.pagopa.register.repository.operation;
 
 import it.gov.pagopa.register.model.operation.ProducersInitiative;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
@@ -9,6 +11,8 @@ public interface ProducersInitiativeRepository
   extends MongoRepository<ProducersInitiative, String> {
 
   List<ProducersInitiative> findByProducerIdOrderByInitiativeNameAsc(String producerId);
+
+  Page<ProducersInitiative> findByInitiativeId(String initiativeId, Pageable pageable);
 
   boolean existsByProducerIdAndInitiativeIdAndEnabledTrue(String producerId, String initiativeId);
 }
